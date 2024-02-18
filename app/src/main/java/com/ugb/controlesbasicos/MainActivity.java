b3ef7354f9b834cbc17573b71bf4fe3030da3cb7
Yajaira <Yajairaclaos40@gmail.com>
2024-02-18 11:10:26 -0600

    Creación de la rama y código del sendor de luz.

--- app/src/main/java/com/ugb/controlesbasicos/MainActivity.java
+++ app/src/main/java/com/ugb/controlesbasicos/MainActivity.java
@@ -2,6 +2,7 @@ package com.ugb.controlesbasicos;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
@@ -22,8 +23,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        tempVal = findViewById(R.id.lblSensorAcelerometro);
-        activarSensorAcelerometro();
+        tempVal = findViewById(R.id.lblSensorLuz);
+        activarSensorLuz();
 
     }
     @Override
@@ -37,18 +38,26 @@ public class MainActivity extends AppCompatActivity {
         super.onPause();
     }
 
-    private void activarSensorAcelerometro(){
+    private void activarSensorLuz(){
         sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
-        sensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        sensor = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
         if(sensor==null){
-            tempVal.setText("Tu dispositivo NO cuenta con el sensor acelerometro.");
+            tempVal.setText("Tu dispositivo NO cuenta con el sensor luz.");
             finish();
         }
         sensorEventListener = new SensorEventListener() {
             @Override
             public void onSensorChanged(SensorEvent event) {
-                tempVal.setText("Acelerometro: X = " + event.values[0] + "; Y = " + event.values[1] + "; Z = " +
-                        event.values[2]);
+                double valor = event.values[0];
+                tempVal.setText("Luz: " + valor);
+
+                if(valor<=2){
+                    getWindow().getDecorView().setBackgroundColor(Color.BLACK);
+                } else if (valor<=50) {
+                    getWindow().getDecorView().setBackgroundColor(Color.RED);
+                } else {
+                    getWindow().getDecorView().setBackgroundColor(Color.YELLOW);
+                }
             }
 
             @Override
