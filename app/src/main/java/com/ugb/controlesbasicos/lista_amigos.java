cb991c8f0171701bd4eff723828708fcc2a7aac1
Yajaira <Yajairaclaos40@gmail.com>
2024-03-15 00:56:17 -0600

    Creacion de la obtencion de datos Amigos

--- app/src/main/java/com/ugb/controlesbasicos/lista_amigos.java
+++ app/src/main/java/com/ugb/controlesbasicos/lista_amigos.java
@@ -3,13 +3,24 @@ package com.ugb.controlesbasicos;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import java.util.ArrayList;
+
 public class lista_amigos extends AppCompatActivity {
     FloatingActionButton btnAgregarAmigos;
+    ListView lts;
+    Cursor cAmigos;
+    amigos misAmigos;
+    DB db;
+    final ArrayList<amigos> alAmigos = new ArrayList<amigos>();
+    final ArrayList<amigos> alAmigosCopy = new ArrayList<amigos>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,9 +33,47 @@ public class lista_amigos extends AppCompatActivity {
                 abrirActividad();
             }
         });
+        obtenerDatosAmigos();
     }
     private void abrirActividad(){
         Intent abrirActividad = new Intent(getApplicationContext(), MainActivity.class);
         startActivity(abrirActividad);
+    }
+    private void obtenerDatosAmigos(){
+        try {
+            alAmigos.clear();
+            alAmigosCopy.clear();
+
+            db = new DB(lista_amigos.this, "", null, 1);
+            cAmigos = db.consultar_amigos();
+
+            if (cAmigos.moveToFirst() ){
+                lts = findViewById(R.id.ltsAmigos);
+                do {
+                    misAmigos = new amigos(
+                            cAmigos.getString(0), //idAmigos
+                            cAmigos.getString(1), //idNombre
+                            cAmigos.getString(2), //idDireccion
+                            cAmigos.getString(3), //idTelefono
+                            cAmigos.getString(4), //idEmail
+                            cAmigos.getString(5) //idDui
+                    );
+                    alAmigos.add(misAmigos);
+                }while (cAmigos.moveToFirst());
+                alAmigosCopy.addAll(alAmigos);
+
+                adaptadorImagenes adImagenes = new adaptadorImagenes(lista_amigos.this, alAmigos);
+                lts.setAdapter(adImagenes);
+
+                registerForContextMenu(lts);
+            }else {
+                mostrarMsg("NO HAY DATOS DE AMIGOS QUE MOSTRAR.");
+            }
+        }catch (Exception e){
+            mostrarMsg("Error al mostrar datos" + e.getMessage());
+        }
+    }
+    private void mostrarMsg(String msg){
+        Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_LONG).show();
     }
 }
