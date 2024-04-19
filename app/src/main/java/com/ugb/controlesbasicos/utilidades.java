3606286282042b712643078719cffe1c1efa702f
Isamar <Yajairaclaros40@gmail.com>
2024-04-19 00:40:15 -0600

    Creacion de la rama registroPeliculasCouchDB

--- app/src/main/java/com/ugb/controlesbasicos/utilidades.java
+++ app/src/main/java/com/ugb/controlesbasicos/utilidades.java
@@ -3,8 +3,8 @@ package com.ugb.controlesbasicos;
 import java.util.Base64;
 
 public class utilidades {
-    static String url_consulta = "http://192.168.134.218:5984/amigos/_design/amigos/_view/amigos";
-    static String url_mto = "http://192.168.134.218:5984/amigos";
+    static String url_consulta = "http://192.168.134.218:5984/peliculas/_design/peliculas/_view/peliculas";
+    static String url_mto = "http://192.168.134.218:5984/peliculas";
     static String user = "Isamar";
     static String passwd = "CLAROSUGB2022";
     static String credencialesCodificadas = Base64.getEncoder().encodeToString((user + ":" + passwd).getBytes());
